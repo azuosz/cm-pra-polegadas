3e224f0cc1673f8cb37ae665e0ab4345a8877108
André Camargo <andrefelipe_camargo@hotmail.com>
2021-02-14 02:58:51 -0300

    Update testetesteapenasteste.py

--- testetesteapenasteste.py
+++ testetesteapenasteste.py
@@ -4,11 +4,9 @@ print(alo)
 if(alo == 1):
     cm = float(input('Indique quantos cm são: '))
     polegada = cm / 2.54
-
     print('{} centimetros da {} polegadas'.format(cm, polegada))
 
 elif(alo == 2):
     polegada1 = float(input('Indique quantas polegadas são: '))
     cm1 = polegada1 * 2.54
-
     print('{} polegadas são {} cm'.format(polegada1, cm1))
