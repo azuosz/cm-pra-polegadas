2e8c2d94433350a0644f8e189a611f3ce0aa7bf5
André Camargo <andrefelipe_camargo@hotmail.com>
2021-02-23 15:20:28 -0300

    Update testetesteapenasteste.py

--- testetesteapenasteste.py
+++ testetesteapenasteste.py
@@ -1,14 +1,14 @@
-alo = int(input('Escolha 1 para converter cm pra polegadas ou 2 para converter polegadas pra cm '))
+alo = int(input('1 para converter cm pra polegadas \n2 para converter polegadas pra cm \n'))
 print(alo)
 
 if(alo == 1):
     cm = float(input('Indique quantos cm são: '))
     polegada = cm / 2.54
-    
-    print('{} centimetros da {} polegadas'.format(cm, polegada))
+
+    print('{} centimetros da {:.2f} polegadas'.format(cm, polegada))
 
 elif(alo == 2):
     polegada1 = float(input('Indique quantas polegadas são: '))
     cm1 = polegada1 * 2.54
-    
+
     print('{} polegadas são {} cm'.format(polegada1, cm1))
